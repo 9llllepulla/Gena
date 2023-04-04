6215e8c58e4f40e1173836138b71f07e437c00df
sergeylyashko <slyashko@mail.ru>
2023-04-04 22:05:21 +0300

    add header in PersonalityGenerators, clean code

--- src/PersonalityGenerators.hs
+++ src/PersonalityGenerators.hs
@@ -1,3 +1,12 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  GHC.PersonalityGenerators
+-- Copyright   :  (c) Sergey Lyashko 2023
+-- License     :  see LICENSE
+--
+-- Generator for personality full names
+--
+-----------------------------------------------------------------------------
 module PersonalityGenerators
   ( cross,
     randomFullNameGen,
@@ -21,16 +30,16 @@ capitalize :: String -> String
 capitalize [] = []
 capitalize (x : xs) = toUpper x : xs
 
+-- генерация случайного имени-фамилии
 randomFullNameGen :: (Int, Int) -> Int -> [String]
-randomFullNameGen (nameLen, lastNameLen) count =
-  let name = randomName nameLen $ nameLen * count
-      lastName = randomName lastNameLen $ lastNameLen * count + nameLen
+randomFullNameGen (nameLen, lastNameLen) offset =
+  let name = randomName nameLen $ nameLen * offset
+      lastName = randomName lastNameLen $ lastNameLen * offset + nameLen
    in cross [name] [lastName]
 
 randomName :: Int -> Int -> String
-randomName count offset = take count (randomRs ('a', 'z') $ mkStdGen $ count * offset)
+randomName charsCount offset = take charsCount (randomRs ('a', 'z') $ mkStdGen $ charsCount * offset)
 
 -- пересечение всех имен и фамилий
 cross :: [Name] -> [LastName] -> [String]
---cross names lastNames = [name ++ " " ++ lastName | name <- names, lastName <- lastNames]
 cross names lastNames = [toString (FullName name lastName :: FullName) | name <- names, lastName <- lastNames]
