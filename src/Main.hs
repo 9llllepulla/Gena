f100920b89259ee1f35232c312ed23f78daec40f
sergeylyashko <slyashko@mail.ru>
2023-04-26 22:59:33 +0300

    change main for use args

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,15 @@
 module Main (main) where
 
-import PhoneOutput
+import System.Environment
+import CommandLine
 
 main :: IO ()
 main = do
-  printRandomPhonesOnConsole 7 3
-  printRandomPhonesToFile 7 3 "3_mobile.txt"
+  args <- getArgs
+  putStrLn $ commandArgs args
+
+{-main :: IO ()
+  main = do
+    printRandomPhonesOnConsole 7 3
+    printRandomPhonesToFile 7 3 "3_mobile.txt"
+    -}
